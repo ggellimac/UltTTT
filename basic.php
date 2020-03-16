17b7418a959625a1fad26c2513a5ebcc3408212f
parkeugene96 <40305854+parkeugene96@users.noreply.github.com>
2020-03-15 22:15:04 -0400

    Add files via upload

--- basic.php
+++ basic.php
@@ -15,7 +15,7 @@
   </div>
   <div class="col-3 button-section">
     <button class="btn">New Game</button><br>
-    <a href = "ultttt.php" class="btn">Change Game Mode</a>
+    <a href = "choose.php" class="btn">Choose Game Mode</a>
     <a href="basicrules.html" target="_self" class="btn">How To Play</a>
     <a href="https://youtu.be/F3ZAWyA-NAc" target="_self" class="btn">YouTube Video</a>
     <a href = "logout.php" class="btn">Sign Out</a>
