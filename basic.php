a606fb2bee148fbaf6c895f98cb320a0093ced2d
parkeugene96 <40305854+parkeugene96@users.noreply.github.com>
2020-04-22 17:30:08 -0400

    Update basic.php

--- basic.php
+++ basic.php
@@ -28,7 +28,7 @@
   </div>
   <div class="col-3 button-section">
     <button class="btn">New Game</button><br>
-    <a href = ".php" class="btn">Choose Game Mode</a>
+    <a href = "choose.php" class="btn">Choose Game Mode</a>
     <a href="basicrules.html" target="_self" class="btn">How To Play</a>
     <a href = "logout.php" class="btn">Sign Out</a>
   </div>
